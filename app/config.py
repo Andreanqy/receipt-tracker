8f665812d03a9a7e48084f533a1ee88731b6e4f1
Андрей Мохов <mohovandrew2@gmail.com>
2026-06-04 12:06:58 +0300

    feat: configure environment variables for database connection

---
+++ app/config.py
@@ -0,0 +1,33 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
+
+class Settings(BaseSettings):
+    # App
+    PROJECT_NAME: str = "Receipt Tracker"
+    DEBUG: bool = False
+
+    # Postgres
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    # Redis
+    REDIS_HOST: str
+    REDIS_PORT: int
+
+    # MinIO
+    MINIO_ROOT_USER: str
+    MINIO_ROOT_PASSWORD: str
+    MINIO_ENDPOINT: str
+    MINIO_BUCKET_NAME: str
+
+    @computed_field
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settigns = Settings()
